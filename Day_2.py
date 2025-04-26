5e82982d96bf39c41ceb5ce96ee6a9a8c498f331
Deepak Pancholi <163425955+Deepak9521@users.noreply.github.com>
2025-04-26 23:15:21 +0530

    Update Day_2.py

--- Day_2.py
+++ Day_2.py
@@ -2,5 +2,5 @@
 
 x = 5
 y = 'Devanath'
-print = (type(x))#This is to show the type of the x like output of this is -->  <class int>
-print = (type(y))#Same thing gose for this one the output is -->  <class str>
+print(type(x))#This is to show the type of the x like output of this is -->  <class int>
+print(type(y))#Same thing gose for this one the output is -->  <class str>
