d707d6061c2920e609216fec73850c4251ab6f92
Deepak Pancholi <163425955+Deepak9521@users.noreply.github.com>
2025-04-26 23:12:53 +0530

    Create Day_2.py

---
+++ Day_2.py
@@ -0,0 +1,6 @@
+# This is day two and still i did not did anything useful
+
+x = 5
+y = 'Devanath'
+print = (type(x))#This is to show the type of the x like output of this is -->  <class int>
+print = (type(y))#Same thing gose for this one the output is -->  <class str>
